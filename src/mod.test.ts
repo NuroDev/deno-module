2753ea9f100c8bc115d948b877f1adfdc4bccd20
nurodev <me@nuro.dev>
2022-05-21 02:09:37 +0100

    Minor `src/` structure tweaks

--- src/mod.test.ts
+++
@@ -1,7 +0,0 @@
-import { assertEquals, assertNotEquals } from 'std/testing/asserts.ts';
-
-import { add } from './mod.ts';
-
-Deno.test('1 + 2 = 3', () => assertEquals(add(1, 2), 3));
-
-Deno.test('1 + 2 != 4', () => assertNotEquals(add(1, 2), 4));
--- src/mod.ts
+++ src/mod.ts
@@ -1,11 +1,1 @@
-/**
- * Add
- *
- * @description Adds 2 provided numbers together & returns them
- *
- * @param {Number} a - First number
- * @param {Number} b - Second number
- */
-export function add(a: number, b: number): number {
-	return a + b;
-}
+export { add } from "./add.ts";
